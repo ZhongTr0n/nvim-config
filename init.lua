536c8d3ea2b660ad16072f6a1dc27db29b4b9ac9
Bruno <bruno@mac.home>
2024-11-11 20:57:28 +0100

    Added lazyvim

--- init.lua
+++ init.lua
@@ -53,3 +53,32 @@ vim.opt.autochdir = true
 -- Display partial commands in the last line of the screen
 vim.opt.showcmd = true
 
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+-- Highlight all matching search terms
+vim.opt.hlsearch = true
+
+-- Show partial matches as you type during search
+vim.opt.incsearch = true
+
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
