ddbcf1ff5dde601a414358633c7ee1a4d45dc3c9
Bruno <bruno@mac.home>
2025-05-07 20:18:31 +0200

    also added geojson and toposjon to auto parsing

--- init.lua
+++ init.lua
@@ -12,8 +12,9 @@ require("colorscheme")
 vim.env.LANG = "en_US.UTF-8"
 vim.o.wrap = true
 
--- Autocommand to format JSON files on open
+
+-- Autocommand to format JSON, topojson, and geojson files on open
 vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*.json",
+  pattern = {"*.json", "*.topojson", "*.geojson"},
   command = "%!python3 -m json.tool",
 })
